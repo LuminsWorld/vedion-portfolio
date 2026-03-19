0abb59568b060896d5c4688258ba534de9e1246f
Austin Tessmer <austintessmer06@gmail.com>
2026-03-20 02:28:57 +0800

    fix: externalize firebase-admin from webpack + outputFileTracingIncludes for Vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Externalize firebase-admin from webpack so it's not bundled (it breaks when bundled)
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('firebase-admin')
+    }
+    return config
+  },
+
+  // Tell Vercel's file tracer to include firebase-admin in the deployment bundle
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**': ['./node_modules/firebase-admin/**/*'],
+    },
+  },
 }
+
 module.exports = nextConfig
