2b9368301f03cf9d96d975ed1e5141ba7055fa90
Austin Tessmer <austintessmer06@gmail.com>
2026-03-20 02:14:09 +0800

    fix: remove webpack externals, fully safe firebaseAdmin init

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Prevent firebase-admin from being bundled by webpack
-      config.externals = [
-        ...(Array.isArray(config.externals) ? config.externals : [config.externals]),
-        'firebase-admin',
-        'firebase-admin/app',
-        'firebase-admin/auth',
-        'firebase-admin/firestore',
-        'firebase-admin/storage',
-      ]
-    }
-    return config
-  },
 }
 module.exports = nextConfig
